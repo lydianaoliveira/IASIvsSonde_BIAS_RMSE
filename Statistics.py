483d3cc1fc79a0e768e2f4addabafe922da3dc2e
Lydiana Oliveira <62446448+lydianaoliveira@users.noreply.github.com>
2024-08-06 10:40:14 -0300

    Statistics.py

--- Statistics.py
+++ Statistics.py
@@ -1,74 +1,90 @@
-#
+# Python program for calculate Bias and RMSE
 
 # import
 import pandas as pd
 import numpy as np
 import math
-from sklearn.metrics import mean_absolute_error as mae
-from sklearn.metrics import mean_squared_error as mse
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from mlxtend.evaluate import bias_variance_decomp
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# abrindo o arquivo
+# open csv file
 df = pd.read_csv('DadosTemperatura.csv')
 df = df.round(2)
-print(df)
 
-'''
-iasiT = df['T IASI']
-sondaT = df['T Sonda']
+# dataframe for temperature data at each pressure level
+# criando novo dataframe para armazenar dados por nivel de pressao
+lst = list(range(60, 1050, 50))
+dic = {chave: [[],[],[], []] for chave in lst} #cria um dicionario {P: [temperaturas]}
 
-# Valores MAX/MIN e média
-#--- IASI
-iasiTMax = np.max(iasiT)
-iasiTMin = np.min(iasiT)
-iasiMedia = round(np.mean(iasiT), 2)
-print(f'IASI valor máximo {iasiTMax}, valor mínimo {iasiTMin} e média {iasiMedia}')
+def nearests(valueP, valueN):
+    return abs(valueN - valueP <= 10)
 
-#--- Sonda
-sondaTMax = np.max(sondaT)
-sondaTMin = np.min(sondaT)
-sondaMedia = round(np.mean(sondaT), 2)
-print(f'Sonda valor máximo {sondaTMax}, valor mínimo {sondaTMin} e média {sondaMedia}')
 
-# Erro médio absoluto
-MAE = mae(df['T Sonda'], df['T IASI'])
-print(f'Erro Médio Absoluto {MAE}')
+for pressure in df['P']:
+    for key in dic.keys():
+        if(nearests(pressure, key)):
+            idx = df[df['P'] == pressure].index[0]
+            tempP = df.iloc[idx]['T Sonda']
+            tempI = df.iloc[idx]['T IASI']
+            if tempI == '--':
+                tempI = np.nan
+            else:
+                tempI = np.float64(tempI)
+            dic[key][0].append(tempP)
+            dic[key][1].append(tempI)
 
-# Erro quadrático médio
-MSE = mse(df['T Sonda'], df['T IASI'])
-print(f'Erro Quadrático Médio {MSE}')
 
-# Root Mean Square Error (RMSE)
-RMSE = math.sqrt(MSE)
-print(f'RMSE {RMSE}')
+dfP = pd.DataFrame(data=dic)
 
-# BIAS
-x = df['T IASI'].to_numpy()
-y = df['T Sonda'].to_numpy()
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
+# Statistics
+def Bias(lstErrors):
+    bias = np.mean(lstErrors)
+    return bias
 
-model = LinearRegression()
-mse, bias, var = bias_variance_decomp(model, x_train, y_train, x_test, y_test, loss='mse', num_rounds=98, random_seed=1)
+def RMSE(lstErrors):
+    rmse = np.sqrt(np.mean([x**2 for x in lstErrors]))
+    return rmse
 
-print('MSE: %.3f' % mse)
-print('Bias: %.3f' % bias)
-print('Variance: %.3f' % var)
+for p in dfP.columns:
+    lstErrors = []
 
-print(40*'-')
-print('')
+    for i in range(len(dfP[p][0])):
+        n = dfP[p][0][i] - dfP[p][1][i]  #observado(sonda) - estimado(iasi)
+        lstErrors.append(n)
+    cleanLstErrors = [x for x in lstErrors if not math.isnan(x)]
 
-# Desvio padrão
-df['Desvio'] = df.std(axis=1)
+    x = Bias(cleanLstErrors)
+    dfP[p][2].append(x)
 
-# Diferença
-df['Diferença'] = df['T Sonda'] - df['T IASI']
+    y = RMSE(cleanLstErrors)
+    dfP[p][3].append(y)
 
-print(df)
-'''
+#---- Bias
+lstBias = []
+for p in dfP.columns:
+    n = dfP[p][2][0]
+    lstBias.append(n)
 
-# Salvando DataFrame em arquivo csv
-#df.to_csv(r'DataTemperaturaIASISonda.csv', index=False)
+#---- RMSE
+lstRMSE = []
+for p in dfP.columns:
+    n = dfP[p][3][0]
+    lstRMSE.append(n)
 
+dfS = pd.DataFrame(list(zip(lst, lstBias, lstRMSE)), columns=['P', 'Bias', 'RMSE'])
+
+# Plot
+sns.set_theme(style="darkgrid")
+fig, ax = plt.subplots(figsize=(12,5))
+plt.scatter(dfS['Bias'], dfS['P'], color='blue', label='Bias')
+plt.plot(dfS['RMSE'], dfS['P'], color='green', label='RMSE')
+plt.gca().invert_yaxis()
+plt.xlim(np.nanmin(dfS['Bias'])-0.5, np.nanmax(dfS['RMSE'])+0.5)
+plt.ylim(np.nanmin(dfS['P']-10), np.nanmax(dfS['P']+10))
+plt.title('Temperature Nonlinear retrievals for IASI-MetOp')
+plt.xlabel('BIAS/RMSE (K)')
+plt.ylabel('P (hPa)')
+plt.legend()
+plt.savefig('BiasRMSE_20082011', dpi=400)
+plt.show()
 
